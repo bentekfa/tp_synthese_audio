8a6f5ab72b490330c00748f8165d1b9ecec0b949
bentekfa <maram.bentekfa@ensea.fr>
2025-11-13 09:39:30 +0100

    test

--- tp_sunthese_audio/Core/Src/main.c
+++ tp_sunthese_audio/Core/Src/main.c
@@ -111,9 +111,9 @@ int main(void)
 
 		//*******QUESTION 1 ********
 		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, GPIO_PIN_SET);
-		HAL_Delay(500);
+		HAL_Delay(1000);
 		HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5, GPIO_PIN_RESET);
-		HAL_Delay(500);
+		HAL_Delay(1000);
 
 		//*******QUESTION 3 ********
 
